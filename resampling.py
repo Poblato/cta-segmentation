de030d0be5cb038231250edb381619345a706e26
Blaise Robson <blaise@blaiserobson.com>
2026-06-22 15:53:36 +0100

    resampling functional stills needs to read params

--- resampling.py
+++ resampling.py
@@ -6,14 +6,16 @@ import scipy
 import matplotlib.pyplot as plt
 
 # TODO: read params from command line
-filepath = Path.cwd().joinpath("1-200")
+inputfilepath = Path.cwd().joinpath("1-200")
+outputfilepath = "91_sampled.img.nii.gz"
+
+specify_shape = True
+target_shape = [256, 256, 275]
 target_pixdim = [1, 1, 1]
 chunk_size = 16
 
-# should labels also be resampled or rescale output back to original size before comparison?
-
 n1_img = nib.load(filename="91.img.nii.gz")
-print(n1_img)
+# print(n1_img)
 
 # Resampling
 data_shape = n1_img.header.get_data_shape()
@@ -27,6 +29,11 @@ pix_dim = n1_img.header.get_zooms()
 
 data = n1_img.dataobj
 #data = n1_img.get_fdata()
+
+if specify_shape:
+    target_pixdim = np.divide(np.multiply(pix_dim, data_shape), target_shape)
+
+# print(target_pixdim)
 
 x_space = np.arange(0,data_shape[0])
 y_space = np.arange(0,data_shape[1])
@@ -43,7 +50,7 @@ samples_per_chunk = np.int32(np.ceil(np.divide(chunk_size, steps[2])))
 resamp_shape[2] = np.int32(samples_per_chunk * (num_chunks-1))
 resamp_shape[2] += np.int32(np.ceil(np.mod(data_shape[2], chunk_size) / steps[2]))
 
-print(resamp_shape)
+# print(resamp_shape)
 resampled_data = np.zeros(shape=resamp_shape)
 
 for i in np.arange(0, num_chunks):
@@ -61,21 +68,22 @@ for i in np.arange(0, num_chunks):
     resampled_data[:,:,after_lb:after_ub] = interp((X, Y, Z))
 
 # plot against original
-fig = plt.figure()
+# fig = plt.figure()
 
-plt.plot(x_space, data[:,0,4], c='k', label='data')
-plt.plot(xx, resampled_data[:,0,2], c='g', label='linear')
+# plt.plot(x_space, data[:,0,4], c='k', label='data')
+# plt.plot(xx, resampled_data[:,0,4], c='g', label='linear')
 
-# ax = fig.add_subplot(projection='3d')
-# ax.scatter(x_space, y_space, data[:,:,4], s=60, c='k', label='data')
-# ax.plot_wireframe(xx, yy, lin_resampled_data[:,:,4], rstride=3, cstride=3,
-#                   alpha=0.4, color='m', label='linear')
-# ax.plot_wireframe(xx, yy, cub_resampled_data[:,:,4], rstride=3, cstride=3,
-#                   alpha=0.4, color='g', label='cubic')
+# plt.legend()
+# plt.show()
 
-plt.legend()
-plt.show()
+# calculate affine from new pixdims
+affine = n1_img.affine
+affine[0:3,0:3] = np.diag(target_pixdim)
+affine[0,0] = -affine[0,0] # flip x axis
 
-# Normalisation
-
-# other things
+# assemble into new Nifti1Image and save
+resampled_img = nib.Nifti1Image(resampled_data, affine=affine, header=n1_img.header)
+resampled_img.header.set_data_shape(resamp_shape)
+resampled_img.header.set_zooms(target_pixdim)
+# print(resampled_img)
+nib.save(resampled_img, outputfilepath)
