set up eval with (maybe) fixed metric calculation

segment.py
@@ -6,12 +6,13 @@ import nibabel as nib
 import torchio as tio
 from pathlib import Path
 from WeightedLoss import WeightedLoss
+import matplotlib.pyplot as plt
 
 # import model params
 # run model on specified file
 # read params from command line
-# 
-model_path = "model_weights/config_0"
+
+model_path = "model_weights_prelim/config_0"
 image_path = "dataset/processed/images/1.img_processed_norm.nii.gz"
 label_path = "dataset/processed/labels/1.label_processed_norm.nii.gz"
 output_path = "segmentation_1.nii.gz"
@@ -26,15 +27,14 @@ subject = tio.Subject(
     image=tio.ScalarImage(Path(image_path)),
     label=tio.LabelMap(Path(label_path))
 )
-
 dataset = tio.SubjectsDataset([subject])
-
 dataloader = tio.SubjectsLoader(
     dataset,
     batch_size=1,
     num_workers=0,
-    shuffle=True,
+    shuffle=False,
 )
+img_file = nib.load(Path(image_path))
 
 with torch.no_grad():
     for batch in dataloader:
@@ -49,7 +49,7 @@ with torch.no_grad():
         probs = torch.sigmoid(logits)
         segmentation = (probs > TH).long()
 
-        acc = (lbl_layer == preds).float().mean().item()
+        acc = (segmentation == lbl_data).float().mean().item()
         tp, fp, fn, tn = smp.metrics.get_stats(segmentation, (lbl_data > 0), mode='binary')
         tp = torch.sum(tp)
         fp = torch.sum(fp)
@@ -59,11 +59,22 @@ with torch.no_grad():
         jaccard = smp.metrics.iou_score(tp, fp, fn, tn)
         precision = smp.metrics.precision(tp, fp, fn, tn)
         recall = smp.metrics.recall(tp, fp, fn, tn)
-
         print(f"acc={acc},dice={dice},tp={tp},fp={fp},fn={fn},tn={tn},jaccard={jaccard},precision={precision},recall={recall}")
 
+# reorder the segmentation back to normal
+segmentation = segmentation.permute(1,2,3,0).squeeze()
 # encode into new nifti image
-segmented_img = nib.Nifti1Image(segmentation, affine=img.affine, header=img.header)
+segmented_img = nib.Nifti1Image(segmentation, affine=img_file.affine, header=img_file.header)
 nib.save(segmented_img, output_path)
 
-# (optional) visualise model output
+# visualise model output
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.voxels(segmentation, facecolors='r')
+
+fig2 = plt.figure()
+lbl = lbl.squeeze().squeeze()
+ax2 = fig2.add_subplot(projection='3d')
+ax2.voxels(lbl, facecolors='r')
+
+plt.show()
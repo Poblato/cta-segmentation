a398faf19aa76875ad7d8b6576d6d00d1bd07a2f
Blaise Robson <blaise@blaiserobson.com>
2026-07-24 11:01:00 +0100

    set up eval with (maybe) fixed metric calculation

---
+++ eval.py
@@ -0,0 +1,75 @@
+import os
+import torch
+import numpy as np
+import segmentation_models_pytorch as smp
+import torchio as tio
+from pathlib import Path
+from WeightedLoss import WeightedLoss
+import logging
+
+# TODO: read params from command line
+
+model_path = "model_weights_prelim/config_0"
+image_path = "dataset/processed/images/1.img_processed_norm.nii.gz"
+label_path = "dataset/processed/labels/1.label_processed_norm.nii.gz"
+output_path = "segmentation_1.nii.gz"
+TH = 0.5
+
+logger = logging.getLogger('eval')
+logger.addHandler(logging.FileHandler("logs/eval.log"))
+logger.setLevel(logging.INFO)
+logger.info("Config Num, Acc, TP, FP, FN, TN, Dice, Jaccard, Precision, Recall")
+
+num_models = 42
+for i in range(num_models):
+    model = smp.from_pretrained(Path(f"model_weights_prelim/config_{i}"))
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    num_images = 200
+    data_list = []
+    for i in range(num_images):
+        subject = tio.Subject(
+            image=tio.ScalarImage(Path(f"dataset/processed/images/{i+1}.img_processed_norm.nii.gz")),
+            label=tio.LabelMap(Path(f"dataset/processed/labels/{i+1}.label_processed_norm.nii.gz"))
+        )
+        data_list.append(subject)
+    dataset = tio.SubjectsDataset(data_list)
+    dataloader = tio.SubjectsLoader(
+        dataset,
+        batch_size=1,
+        num_workers=0,
+        shuffle=False,
+    )
+
+    tp, fp, fn, tn, acc = 0, 0, 0, 0, 0
+
+    with torch.no_grad():
+        for batch in dataloader:
+            img = batch["image"][tio.DATA].to(device)
+            lbl = batch["label"][tio.DATA].to(device)
+
+            img_data = img.squeeze(1).permute(3, 0, 1, 2)
+            lbl_data = lbl.squeeze(1).permute(3, 0, 1, 2)
+
+            logits = model(img_data)
+
+            probs = torch.sigmoid(logits)
+            segmentation = (probs > TH).long()
+
+            acc += (segmentation == lbl_data).float().mean().item()
+            tp_t, fp_t, fn_t, tn_t = smp.metrics.get_stats(segmentation, (lbl_data > 0), mode='binary')
+            tp += torch.sum(tp_t)
+            fp += torch.sum(fp_t)
+            fn += torch.sum(fn_t)
+            tn += torch.sum(tn_t)
+
+    acc /= num_images
+    dice = smp.metrics.f1_score(tp, fp, fn, tn)
+    jaccard = smp.metrics.iou_score(tp, fp, fn, tn)
+    precision = smp.metrics.precision(tp, fp, fn, tn)
+    recall = smp.metrics.recall(tp, fp, fn, tn)
+    line = f"{i},{acc},{tp},{fp},{fn},{tn},{dice},{jaccard},{precision},{recall}" 
+    print(line)
+    logger.info(line)
--- segment.py
+++ segment.py
@@ -6,12 +6,13 @@ import nibabel as nib
 import torchio as tio
 from pathlib import Path
 from WeightedLoss import WeightedLoss
+import matplotlib.pyplot as plt
 
 # import model params
 # run model on specified file
 # read params from command line
-# 
-model_path = "model_weights/config_0"
+
+model_path = "model_weights_prelim/config_0"
 image_path = "dataset/processed/images/1.img_processed_norm.nii.gz"
 label_path = "dataset/processed/labels/1.label_processed_norm.nii.gz"
 output_path = "segmentation_1.nii.gz"
@@ -26,15 +27,14 @@ subject = tio.Subject(
     image=tio.ScalarImage(Path(image_path)),
     label=tio.LabelMap(Path(label_path))
 )
-
 dataset = tio.SubjectsDataset([subject])
-
 dataloader = tio.SubjectsLoader(
     dataset,
     batch_size=1,
     num_workers=0,
-    shuffle=True,
+    shuffle=False,
 )
+img_file = nib.load(Path(image_path))
 
 with torch.no_grad():
     for batch in dataloader:
@@ -49,7 +49,7 @@ with torch.no_grad():
         probs = torch.sigmoid(logits)
         segmentation = (probs > TH).long()
 
-        acc = (lbl_layer == preds).float().mean().item()
+        acc = (segmentation == lbl_data).float().mean().item()
         tp, fp, fn, tn = smp.metrics.get_stats(segmentation, (lbl_data > 0), mode='binary')
         tp = torch.sum(tp)
         fp = torch.sum(fp)
@@ -59,11 +59,22 @@ with torch.no_grad():
         jaccard = smp.metrics.iou_score(tp, fp, fn, tn)
         precision = smp.metrics.precision(tp, fp, fn, tn)
         recall = smp.metrics.recall(tp, fp, fn, tn)
-
         print(f"acc={acc},dice={dice},tp={tp},fp={fp},fn={fn},tn={tn},jaccard={jaccard},precision={precision},recall={recall}")
 
+# reorder the segmentation back to normal
+segmentation = segmentation.permute(1,2,3,0).squeeze()
 # encode into new nifti image
-segmented_img = nib.Nifti1Image(segmentation, affine=img.affine, header=img.header)
+segmented_img = nib.Nifti1Image(segmentation, affine=img_file.affine, header=img_file.header)
 nib.save(segmented_img, output_path)
 
-# (optional) visualise model output
+# visualise model output
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.voxels(segmentation, facecolors='r')
+
+fig2 = plt.figure()
+lbl = lbl.squeeze().squeeze()
+ax2 = fig2.add_subplot(projection='3d')
+ax2.voxels(lbl, facecolors='r')
+
+plt.show()
